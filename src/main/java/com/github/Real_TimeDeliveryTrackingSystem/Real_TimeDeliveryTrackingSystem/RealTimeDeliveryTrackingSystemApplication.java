bec0bd30e3ffea0333d2b122c2726c889fdc21d1
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2024-11-04 18:32:01 -0300

    initial commit

---
+++ src/main/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/RealTimeDeliveryTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.github.Real_TimeDeliveryTrackingSystem.Real_TimeDeliveryTrackingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDeliveryTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDeliveryTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/RealTimeDeliveryTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Real_TimeDeliveryTrackingSystem.Real_TimeDeliveryTrackingSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDeliveryTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
