ed2cead32262a138d68168ad674f485f6d2facf9
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2024-11-11 16:20:49 -0300

    Implementation of the Swagger operation into the vehicle controller

--- src/main/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/controller/contract/VehicleControllerContract.java
+++ src/main/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/controller/contract/VehicleControllerContract.java
@@ -22,43 +22,47 @@ public interface VehicleControllerContract {
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-//    @Operation(summary = "Register a new Course",
-//            description = "Creates a new course and returns the created course.",
-//            tags = "Courses")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "201", description = "Successful operation",
-//                    content = @Content(schema = @Schema(implementation = CourseResponse.class))),
-//            @ApiResponse(responseCode = "404", description = "Course Not Found or Field Not Found",
-//                    content = @Content),
-//            @ApiResponse(responseCode = "500", description = "Internal server error",
-//                    content = @Content)
-//    })
+    @Operation(summary = "Register a new Vehicle",
+            description = "Creates a new vehicle and returns the created vehicle.",
+            tags = "Vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Successful operation",
+                    content = @Content(schema = @Schema(implementation = VehicleVO.class))),
+            @ApiResponse(responseCode = "400", description = "Will throw Invalid License Plate or Duplicated License Plate",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Will Throw Vehicle Not Found, Field Not Found, License Plate Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     ResponseEntity<VehicleVO> create(@RequestBody VehicleVO vehicleVO) throws InstantiationException, IllegalAccessException, NoSuchFieldException;
 
 
     @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-//    @Operation(summary = "Update a Course",
-//            description = "Updates an existing course and returns the updated course.",
-//            tags = "Courses")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "Successful operation",
-//                    content = @Content(schema = @Schema(implementation = VehicleVO.class))),
-//            @ApiResponse(responseCode = "404", description = "Course Not Found or Field Not Found",
-//                    content = @Content),
-//            @ApiResponse(responseCode = "500", description = "Internal server error",
-//                    content = @Content)
-//    })
+    @Operation(summary = "Update a Vehicle",
+            description = "Update a vehicle and returns the updated vehicle.",
+            tags = "Vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = @Content(schema = @Schema(implementation = VehicleVO.class))),
+            @ApiResponse(responseCode = "400", description = "Will throw Invalid License Plate or Duplicated License Plate",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Will Throw Vehicle Not Found, Field Not Found, License Plate Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     ResponseEntity<VehicleVO> update(@RequestBody VehicleVO vehicleVO) throws NoSuchFieldException, IllegalAccessException;
 
 
     @GetMapping(value = "/{id}")
-    @Operation(summary = "Find a single Course",
-            description = "Finds a course by its ID and returns it.",
-            tags = "Courses")
+    @Operation(summary = "Find a single vehicle",
+            description = "Finds a vehicle by its ID and returns it.",
+            tags = "Vehicles")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successful operation",
                     content = @Content(schema = @Schema(implementation = VehicleVO.class))),
-            @ApiResponse(responseCode = "404", description = "Course Not Found or Field Not Found",
+            @ApiResponse(responseCode = "404", description = "Will Throw Vehicle Not Found or Field Not Found",
                     content = @Content),
             @ApiResponse(responseCode = "500", description = "Internal server error",
                     content = @Content)
@@ -67,31 +71,45 @@ public interface VehicleControllerContract {
 
 
     @GetMapping("/findByLicensePlate/{licensePlate}")
-//    @Operation(summary = "Find All Courses",
-//            description = "Finds all courses in the system and returns a paginated list.",
-//            tags = "Courses")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "200", description = "Successful operation",
-//                    content = @Content(schema = @Schema(implementation = VehicleVO.class)))
-
+    @Operation(summary = "Find a vehicle by its license plate",
+            description = "Find a vehicle by its license plate and returns it.",
+            tags = "Vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = @Content(schema = @Schema(implementation = VehicleVO.class))),
+            @ApiResponse(responseCode = "400", description = "Will throw Invalid License Plate or Duplicated License Plate",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Will Throw Vehicle Not Found, Field Not Found, License Plate Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     ResponseEntity<VehicleVO> findByLicensePlate(@PathVariable("licensePlate") String licensePlate);
 
     @GetMapping
+    @Operation(summary = "Find All vehicles",
+            description = "Finds all vehicles in the system and returns a paginated list.",
+            tags = "Vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation",
+                    content = @Content(schema = @Schema(implementation = VehicleVO.class)))
+
+    })
     ResponseEntity<Page<VehicleVO>> findAll(@PageableDefault(size = 20, page = 0) Pageable pageable);
 
 
     @DeleteMapping(value = "/{id}")
-//    @Operation(summary = "Delete a Course",
-//            description = "Deletes a course by its ID.",
-//            tags = "Courses")
-//    @ApiResponses(value = {
-//            @ApiResponse(responseCode = "204", description = "Successful operation, will return a not content",
-//            content = @Content),
-//            @ApiResponse(responseCode = "404", description = "Course Not Found",
-//                    content = @Content),
-//            @ApiResponse(responseCode = "500", description = "Internal server error",
-//                    content = @Content)
-//    })
+    @Operation(summary = "Delete a vehicle",
+            description = "Deletes a vehicle by its ID.",
+            tags = "Vehicles")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Successful operation, will return a not content",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Will throw Vehicle Not Found",
+                    content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error",
+                    content = @Content)
+    })
     ResponseEntity<Void> delete(@PathVariable("id") String id);
 }
 
