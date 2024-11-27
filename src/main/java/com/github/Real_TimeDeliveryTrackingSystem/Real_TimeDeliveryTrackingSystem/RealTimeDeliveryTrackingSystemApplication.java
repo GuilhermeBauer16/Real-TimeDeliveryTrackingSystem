9a5ccca54c79452de1085df5c8798a56f9de95e6
GuilhermeBauer16 <guilhermebauer16@gmail.com>
2024-11-27 16:05:34 -0300

    feati12: fixing the broken controller integrations tests

--- src/main/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/RealTimeDeliveryTrackingSystemApplication.java
+++ src/main/java/com/github/Real_TimeDeliveryTrackingSystem/Real_TimeDeliveryTrackingSystem/RealTimeDeliveryTrackingSystemApplication.java
@@ -2,12 +2,8 @@ package com.github.Real_TimeDeliveryTrackingSystem.Real_TimeDeliveryTrackingSyst
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.web.config.EnableSpringDataWebSupport;
-
-import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
-@EnableSpringDataWebSupport(pageSerializationMode = VIA_DTO)
 public class RealTimeDeliveryTrackingSystemApplication {
 
     public static void main(String[] args) {
